c6010b9e3303b05e77f08d383db1dc47ef997317
Christian van Ooy <cvodutch@gmail.com>
2018-06-27 00:55:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string. toLowerCase())
 }
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase)()=== string {
+    return "I can't here you!";
+  } else if(string.toUpperCase)()===string{
+    return "YES INDEED";
+    else if (string=== "I love you, Grandma.") {
+      return "I love you, too."
+    }
+  }
+  }
