e298c7edceca5f038d48acc82f00cdccec2dbd87
Christian van Ooy <cvodutch@gmail.com>
2018-06-27 00:57:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function sayHiToGrandma(string){
     return "I can't here you!";
   } else if(string.toUpperCase)()=== string){
     return "YES INDEED";
-    else if (string=== "I love you, Grandma.") {
-      return "I love you, too."
+  }  else if (string=== "I love you, Grandma.") {
+      return "I love you, too.";
     }
-  }
-  }
+  
+  
